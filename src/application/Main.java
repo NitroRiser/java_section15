f6c865fb45c59c9334ef0c39c17ed7262f77d6f3
NitroRiser <jonathanfariastech@gmail.com>
2022-08-02 20:19:19 -0300

    Stack trace

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,7 +4,22 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+        method1();
+
+        System.out.println("Fim do programa.");
+    }
+
+    public static void method1(){
+        System.out.println("**** METHOD 1 START ****");
+        method2();
+        System.out.println("**** METHOD 1 END ****");
+
+    }
+
+    public static void method2(){
+        System.out.println("**** METHOD 2 START ****");
         Scanner sc = new Scanner(System.in);
         try {
             String[] vect = sc.nextLine().split(" ");
@@ -12,11 +27,13 @@ public class Main {
             System.out.println(vect[position]);
         } catch (ArrayIndexOutOfBoundsException e){
             System.out.println("Invalid position!");
+            e.printStackTrace();
+            sc.next();
         } catch (InputMismatchException e){
             System.out.println("Input error!");
         }
-        System.out.println("Fim do programa.");
-
+        System.out.println("**** METHOD 2 END ****");
         sc.close();
     }
+
 }
